1120be82de26c38113cbb955bcc149897492af30
kwiessle <kiefer@iMac-de-Kiefer.local>
2018-08-11 17:25:41 +0200

    clean

--- srcs/tools.c
+++ srcs/tools.c
@@ -6,7 +6,7 @@
 /*   By: kiefer <kiefer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/08 17:14:07 by kiefer            #+#    #+#             */
-/*   Updated: 2018/08/11 15:07:44 by kwiessle         ###   ########.fr       */
+/*   Updated: 2018/08/11 17:23:27 by kwiessle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,28 +58,4 @@ int     little_endian(uint32_t h)
     b2 = (h & 0x00FF0000) >> 16;
     b3 = (h & 0xFF000000) >> 24;
     return b0 << 24 | b1 << 16 | b2 << 8 | b3 << 0;
-}
-
-static long	max_power(long nb, int base)
-{
-	long max;
-
-	max = base;
-	while (max < nb)
-		max *= base;
-	if (max > nb)
-		max /= base;
-	return (max);
-}
-
-void    print_hex(unsigned int n)
-{
-    static char     hex[16] = "0123456789abcdef";
-    long            power = max_power(n, 16);
-
-    while (power > 0) {
-        ft_putchar(hex[n / power]);
-        n %= power;
-        power = power / 16;
-    }
 }
